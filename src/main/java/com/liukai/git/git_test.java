f33e22bb00446ae0f5527cbef3f65e6709434ce8
liukai1223 <75991470+liukai1223@users.noreply.github.com>
2022-02-20 20:01:06 +0800

    pull commit

--- src/main/java/com/liukai/git/git_test.java
+++ src/main/java/com/liukai/git/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
